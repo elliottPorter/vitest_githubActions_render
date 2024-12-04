7a412ac1a67f07be2556c1fccfe564c8c362daf3
Elliott Porter <elliott@weblake.co.uk>
2024-12-04 13:14:28 +0000

    node install with package.json

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+const calcTotalPrice = (price, quantity) => price * quantity;
+
 // sample endpoint
 app.get('/api/sample-endpoint', (req, res) => {
-
-// send the response
-res.status(200).json( { message: 'Hello from the sample endpoint'});
+    // send the response
+    res.status(200).json( { message: `Hello from the sample endpoint and the price is ${calcTotalPrice(2,3)}`});
 })
 
 app.listen(port, () =>{
